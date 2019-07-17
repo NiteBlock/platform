420e46da9eaa6b415585e63ad8077b292b699649
niteblock <you@example.com>
2019-07-17 10:00:31 +0200

    add stuff

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const express = require("express")
+require("dotenv").config({"path":"./variables.env"})
 const mongo = require("mongoose")
 const bodyParser = require("body-parser")
 
+const app = express()
+
+mongo.connect(process.env.DATABASE)
+
+mongo.connection.on("error", function(e){
+    console.error(e)
+})
+
+app.use(bodyParser())
+
+app.get("/test", function(req,res){
+    res.send("Connected.")
+})
+
+app.listen(process.env.PORT, function(){
+    console.log(`Process started on ${process.env.PORT}`)
+})
