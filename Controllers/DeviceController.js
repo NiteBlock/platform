847a287b0d8ec6857ec82db76e7a51fdb8d38d7f
niteblock <niteblock@gmail.com>
2019-07-25 11:02:09 +0200

    mail fix

--- Controllers/DeviceController.js
+++ Controllers/DeviceController.js
@@ -90,7 +90,7 @@ module.exports.updateDeviceColour = function (req, res) {
 
 module.exports.sendEmail = function(req, res){
     const deviceId = req.body.id
-    ModelDevice.findByID(deviceId).then(function (d) {
+    ModelDevice.findById(deviceId).then(function (d) {
         name = d.name
         status = d.status
     }).catch(function(err){
